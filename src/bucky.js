5c2aa1c2a625b12bf449a8010301b66aee0485cc
C Sonal <csonal@thoughtworks.com>
2018-01-15 16:09:56 +0530

    understanding object factory in node js

---
+++ src/bucky.js
@@ -0,0 +1,6 @@
+var movies = require('./movies');
+
+
+var buckyMovie=movies();
+buckyMovie.favMovies="YJHDdddw";
+console.log("movie is "+buckyMovie.favMovies);
---
+++ src/emily.js
@@ -0,0 +1,6 @@
+var movies = require('./movies');
+
+
+var emilyMovie=movies();
+emilyMovie.favMovies="YJHD";
+console.log("movie is "+emilyMovie.favMovies);
